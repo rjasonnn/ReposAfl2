5db9e7d010e7be78efa9bfe30f59a9dcc7bd94a9
sbudiman12 <146686340+sbudiman12@users.noreply.github.com>
2023-11-10 21:46:52 +0700

    hampir selesai

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,46 +15,86 @@
     </section>
 
     <style>
+
+        .imgBox {
+            
+            min-width: 250px;
+            height: 250px;
+            background: url(https://openailabsprodscus.blob.core.windows.net/private/user-ZNEYqYk30hxScGYOdCfF9dgj/generations/generation-vYbvuvJ4Iz1me4L4DDEPussG/image.webp?st=2023-11-10T13%3A07%3A26Z&se=2023-11-10T15%3A05%3A26Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/webp&skoid=15f0b47b-a152-4599-9e98-9cb4a58269f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-11-10T02%3A26%3A41Z&ske=2023-11-17T02%3A26%3A41Z&sks=b&skv=2021-08-06&sig=5oB7cnkWI6yVWoznEH/FFcQLeBebiR2mfp7mz8yTp7w%3D)
+            ; background-size: cover;
+            box-shadow: 0 5px 15px rgba(0, 0, 0, 0.25);
+            margin: 2rem;
+
+        }
        
+       .rounded img{
+            object-fit: cover;
+            width: 260px;
+
+
+            
+       }
     
     </style>
-    <div class="content-aside article">
-        <div class="content" id="content">
 
-             
-            
+
+
+    <div class="content-aside container article">
+        <div class="d-flex flex-wrap" id="content">
+
+            <div class="imgBox col articles rounded-4">
+                
+            </div>
+
+             <div class="articles rounded-4 imgBox col" style="background: url(https://openailabsprodscus.blob.core.windows.net/private/user-ZNEYqYk30hxScGYOdCfF9dgj/generations/generation-GwQikSn9YWIUVm69Gr9fb36X/image.webp?st=2023-11-10T12%3A40%3A10Z&se=2023-11-10T14%3A38%3A10Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/webp&skoid=15f0b47b-a152-4599-9e98-9cb4a58269f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-11-09T17%3A30%3A10Z&ske=2023-11-16T17%3A30%3A10Z&sks=b&skv=2021-08-06&sig=scZCPKNETan5PYd4O8yqvkmW5ZxLjNuk4v35y5TbuPQ%3D); background-size:cover">
+                
+             </div>
+
            
 
+             <div class="imgBox articles rounded-4 col" style="background:url(https://openailabsprodscus.blob.core.windows.net/private/user-ZNEYqYk30hxScGYOdCfF9dgj/generations/generation-pqshofifLJZ2gyGTvfPEwJL3/image.webp?st=2023-11-10T13%3A17%3A57Z&se=2023-11-10T15%3A15%3A57Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/webp&skoid=15f0b47b-a152-4599-9e98-9cb4a58269f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-11-10T10%3A38%3A45Z&ske=2023-11-17T10%3A38%3A45Z&sks=b&skv=2021-08-06&sig=DPh7wJAM2TroFVmVheSGOwtfBablns6Bs6D6vah663Q%3D); background-size:cover;">
+
+             </div>
+
+             <div class="imgBox articles rounded-4 col" style="background:url(https://openailabsprodscus.blob.core.windows.net/private/user-ZNEYqYk30hxScGYOdCfF9dgj/prompts/prompt-64XW3uSksH0AIkj8zbMpegEA/image.webp?st=2023-11-10T13%3A17%3A57Z&se=2023-11-10T15%3A15%3A57Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/webp&skoid=15f0b47b-a152-4599-9e98-9cb4a58269f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-11-10T10%3A38%3A45Z&ske=2023-11-17T10%3A38%3A45Z&sks=b&skv=2021-08-06&sig=3OPDGAmwhbV3UDMKwHBXiUWgcrmza0i5l6RP5I4rs34%3D); background-size:cover;">
+
+             </div>
+            
+             
+
+        </div>
+
+        <div class="m-4 row">
+
+            <div class="col">
+                <h3>Mengapa Arnold Gym: </h3>
+                <p>Pilih gym kami karena kebersihannya yang terjaga, suasana yang menenangkan, dan biaya yang terjangkau. Kami berkomitmen memberikan pengalaman kebugaran yang berkualitas dengan staf yang ramah dan program pelatihan yang disesuaikan untuk membantu Anda mencapai tujuan kesehatan dan kebugaran Anda. Bergabunglah sekarang dan nikmati keuntungan kebugaran dengan harga yang bersahabat!</p>
+
+            </div>
+
+            <div class="col" style="height: 100px;">
+                <aside class="articles">
+                    <h2>Join Us Now!</h2>
+                    <ul>
+                        <li><a href="">Instagram</a></li>
+                        <li><a href="">WhatsApp</a></li>
+                        <li><a href="">Line</a>
+                        </li>
+                        <li><a href="">Telegram</a></li>
+                    </ul>
+            
+                </aside>
+              </div>
         </div>
        
     </div>
 
-    <aside class="articles">
-        <h2>Contact Us</h2>
-        <ul>
-            <li><a href="">Instagram</a></li>
-            <li><a href="">WhatsApp</a></li>
-            <li><a href="">Line</a>
-            </li>
-            <li><a href="">Telegram</a></li>
-        </ul>
+ 
 
-    </aside>
-
-    <div class="container text-center articles">
-        <div class="row gx-5">
-            @foreach ($challenges as $challenge)
-                <div class="article col m-4">
-                    <img src="{{ $challenge->image }}" alt="{{ $challenge->judul }}">
-                    <h3>{{ $challenge->judul }} Challenge</h3>
-                    <p>{{ $challenge->deskripsi }}</p>
-                </div>
-            @endforeach
-        </div>
-    </div>
+    
 
 
-    <div class="row articles">
+    <div class="row articles" style="padding-top: 5rem">
         <div class="article col m-4" colspan="2">
             <h3>Challenge Yourself: Discover a Variety of Challenges at Our Gym </h3>
             <p>Di gym kami, Anda akan menemukan beragam tantangan yang dapat diikuti untuk membantu Anda mencapai tujuan
@@ -70,6 +110,18 @@
             style="background: url(https://img.freepik.com/premium-vector/people-doing-gym-exercise-with-instructor_18660-558.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1698969600&semt=ais); background-repeat:no-repeat; background-size:cover;">
 
             <a href="challenges" class="p-3">View More</a>
+        </div>
+    </div>
+
+    <div class="container text-center articles " style="">
+        <div class="row gx-5">
+            @foreach ($challenges as $challenge)
+                <div class="article col m-4">
+                    <img src="{{ $challenge->image }}" alt="{{ $challenge->judul }}">
+                    <h3>{{ $challenge->judul }} Challenge</h3>
+                    <p>{{ $challenge->deskripsi }}</p>
+                </div>
+            @endforeach
         </div>
     </div>
 
